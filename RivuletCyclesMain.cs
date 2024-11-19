819d37fcff98b2fade8f728b9cc761b32960b001
Ikepike <97116467+Ikepik3@users.noreply.github.com>
2024-11-19 12:13:11 +0100

    Add files via upload

---
+++ RivuletCyclesMain.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using BepInEx;
+using RWCustom;
+using UnityEngine;
+using IL;
+using On;
+
+namespace GiveSlugCatRivuletCycles
+{
+    [BepInPlugin(PLUGIN_GUID, PLUGIN_NAME, PLUGIN_VERSION)]
+    public class RivuletCyclesMain : BaseUnityPlugin
+    {
+        public const string PLUGIN_GUID = "ikepike.RivuletCyclesForAllSlugcats";
+        public const string PLUGIN_NAME = "Rivulet cycles for all slugcats";
+        public const string PLUGIN_VERSION = "1.0.0";
+
+        public void OnEnable()
+        {
+            On.RainCycle.Update += RainUpdateHook;
+            
+        }
+
+        void RainUpdateHook(On.RainCycle.orig_Update orig, RainCycle rain)
+        {
+            //10560
+            var timeLeft = 10560;
+
+            rain.cycleLength = timeLeft;
+            return;
+        }
+    }
+}
---
+++ player.cs
@@ -0,0 +1,15 @@
+﻿using IL;
+using On;
+
+using UnityEngine;
+
+namespace GiveSlugCatRivuletCycles
+{
+    public partial class GiveSlugCatRivuletCycles
+    {
+        private void PlayerOnUpdate(On.Player.orig_Update orig, Player self, bool eu, RainCycle rain)
+        {
+        
+        }
+    }
+}
