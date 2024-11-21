5ba7ef2f3cdc0eb7099d08976220fc3202f1cc6e
ikepik3 <ike.de.vlogger@gmail.com>
2024-11-21 13:10:10 +0100

    Fixed issue where rain was not able to kill the player (there is an if statement where in an certain amout of time, the rain will just start to fall down really fast)

--- RivuletCyclesMain.cs
+++ RivuletCyclesMain.cs
@@ -9,9 +9,17 @@ using RWCustom;
 using UnityEngine;
 using IL;
 using On;
+using Debug = UnityEngine.Debug;
+using System.Security.Permissions;
+using System.Security;
+
+#pragma warning disable CS0618
+[module: UnverifiableCode]
+[assembly: SecurityPermission(SecurityAction.RequestMinimum, SkipVerification = true)]
 
 namespace GiveSlugCatRivuletCycles
 {
+    
     [BepInPlugin(PLUGIN_GUID, PLUGIN_NAME, PLUGIN_VERSION)]
     public class RivuletCyclesMain : BaseUnityPlugin
     {
@@ -34,7 +42,22 @@ namespace GiveSlugCatRivuletCycles
             var timeLeft = 6000; // the time of a rivulet cycle (which is 2 minutes and 30 seconds)
 
             rain.cycleLength = timeLeft; // Give the cycleLength the time of said cycle
+            
             rain.timer += 1; // assuming the code always run per tick, add one tick to the timer, a second in rain world is 20 ticks.
+
+            Debug.Log("test");
+            Debug.Log(rain.deathRainHasHit);
+
+            if (rain.timer == timeLeft+1000)
+            {
+                rain.deathRainHasHit = true;
+                rain.RainHit();
+
+                
+
+                Debug.Log("rainIsComing");
+                Debug.Log(rain.deathRainHasHit);
+            }
         }
     }
 }
